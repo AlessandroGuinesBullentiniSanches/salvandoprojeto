4eb35c161df3188956b426066f993539a2b4b417
alessandro <alessandroguines@gmail.com>
2024-03-15 08:33:53 -0300

    dss

--- 1503.py
+++ 1503.py
@@ -1,8 +1,38 @@
 print('Aula de salvar')
+print('apayaye')
 
 
+class Calculadora:
+    def __init__(self, *args):
+        self.args = args
 
 
+    def somar(self):
+        print(sum(self.args))
 
 
+    def subtrair(self):
+        c = 0
+        for arg in self.args:
+            c-= arg
 
+        print(c+ 2*self.args[0])
+
+    def mul(self):
+        c = 1
+        for arg in self.args:
+            c*=arg
+        print(c)
+
+
+    def div(self):
+        c = self.args[0]
+        for arg in self.args:
+            c/=arg
+        print(c*self.args[0])
+
+
+c = Calculadora(10, 4, .5)
+
+
+c.div()
