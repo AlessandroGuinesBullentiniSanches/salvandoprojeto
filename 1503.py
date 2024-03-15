28e83603090e796df7f19c0820a2864edb3a3cb9
alessandro <alessandroguines@gmail.com>
2024-03-15 09:03:54 -0300

    dss

--- 1503.py
+++ 1503.py
@@ -1,7 +1,3 @@
-print('Aula de salvar')
-print('apayaye')
-
-
 class Calculadora:
     def __init__(self, *args):
         self.args = args
@@ -32,7 +28,42 @@ class Calculadora:
         print(c*self.args[0])
 
 
-c = Calculadora(10, 4, .5)
 
 
-c.div()
+lista = []
+lista2 = ['s','S', 'n','N']
+n = ['n','N']
+while True:
+    numero = float(input('Digite um numero: '))
+    lista.append(numero)
+    yn = str(input('Deseja continuar[s/n]: '))
+    while yn not in lista2:
+        print('Digite apenas s ou n')
+        yn = str(input('Deseja continuar[s/n]: '))
+    if yn in n:
+        break
+
+operacoes = ['+','-','*','/']
+while True:
+    operacao = str(input('Qual operacao realizar com os numeros  (+,-,*,/): '))
+    if operacao not in operacoes:
+        print('digite apenas + - * / ')
+    elif operacao in operacoes:
+        break
+
+
+c = Calculadora(*lista)
+
+if operacao == '+':
+    c.somar()
+
+elif operacao == '-':
+    c.subtrair()
+
+elif operacao == '/':
+    c.div()
+
+elif operacao == '*':
+    c.mul()
+
+
